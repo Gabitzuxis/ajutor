754dca3be9412ea629b3419220c4d68fb3edf539
Gabitzuxis <79571802+Gabitzuxis@users.noreply.github.com>
2026-03-06 17:17:53 +0200

    Update voice_engine.py

--- backend/voice_engine.py
+++ backend/voice_engine.py
@@ -1,1 +1,20 @@
+import subprocess
 
+def generate_voice(script, speed):
+
+    with open("script.txt","w") as f:
+        f.write(script)
+
+    subprocess.run([
+        "espeak",
+        "-s",
+        str(int(170*speed)),
+        "-v",
+        "ro",
+        "-f",
+        "script.txt",
+        "-w",
+        "voice.wav"
+    ])
+
+    return "voice.wav"
