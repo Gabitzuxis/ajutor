7552a552c06c7a88ba6153d6a94b92a45ece62ea
Gabitzuxis <79571802+Gabitzuxis@users.noreply.github.com>
2026-03-06 17:18:48 +0200

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,1 +1,16 @@
+async function login(){
 
+let user=document.getElementById("user").value
+let pass=document.getElementById("pass").value
+
+let r=await fetch("/login",{
+method:"POST",
+headers:{'Content-Type':'application/json'},
+body:JSON.stringify({username:user,password:pass})
+})
+
+let data=await r.json()
+
+if(data.status=="ok")
+location.href="dashboard.html"
+}
