18227d572cd30bb9e255e94477221fa7960dc733
Gabitzuxis <79571802+Gabitzuxis@users.noreply.github.com>
2026-03-06 17:18:02 +0200

    Update image_engine.py

--- backend/image_engine.py
+++ backend/image_engine.py
@@ -1,1 +1,26 @@
+from PIL import Image
+import random
 
+def generate_images(prompts):
+
+    images=[]
+
+    for i,p in enumerate(prompts):
+
+        img = Image.new(
+            "RGB",
+            (1080,1920),
+            (
+                random.randint(0,255),
+                random.randint(0,255),
+                random.randint(0,255)
+            )
+        )
+
+        path=f"img{i}.png"
+
+        img.save(path)
+
+        images.append(path)
+
+    return images
