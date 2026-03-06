70d2d62a3b295539848c77664e3f3644831da39c
Gabitzuxis <79571802+Gabitzuxis@users.noreply.github.com>
2026-03-06 17:18:56 +0200

    Update dashboard.js

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,1 +1,26 @@
+async function generate(){
 
+let script=document.getElementById("script").value
+
+let prompts=document
+.getElementById("prompts")
+.value
+.split("\n")
+
+let speed=document.getElementById("speed").value
+
+let r=await fetch("/generate",{
+method:"POST",
+headers:{'Content-Type':'application/json'},
+body:JSON.stringify({
+script:script,
+prompts:prompts,
+speed:speed
+})
+})
+
+document.getElementById("download").style.display="block"
+
+document.getElementById("download").href="/download"
+
+}
