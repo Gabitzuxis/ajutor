02b12688c97bb3402f25f0af2c4fd0e3b630161b
Gabitzuxis <79571802+Gabitzuxis@users.noreply.github.com>
2026-03-06 17:21:49 +0200

    Update audio.py

--- backend/utils/audio.py
+++ backend/utils/audio.py
@@ -1,1 +1,19 @@
+import subprocess,json
 
+def get_audio_duration(file):
+
+    r=subprocess.run(
+        [
+            "ffprobe",
+            "-v","error",
+            "-show_entries","format=duration",
+            "-of","json",
+            file
+        ],
+        capture_output=True,
+        text=True
+    )
+
+    data=json.loads(r.stdout)
+
+    return float(data["format"]["duration"])
